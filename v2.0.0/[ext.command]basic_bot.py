62ceaf63641e1e72424f4dcb5606476e1d4920dd
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-05-08 15:10:42 +0900

    Create [ext.command]basic_bot.py

---
+++ v2.0.0/[ext.command]basic_bot.py
@@ -0,0 +1,25 @@
+# discord.ext.commandの一番最低限なBot
+
+import discord
+from discord.ext import commands
+import random
+
+description = ''''''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command()
+async def ping(ctx):
+  await ctx.send("pong")
+
+bot.run('token')
